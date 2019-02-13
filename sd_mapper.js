d4d6a9635982341249c91c7852221653b43601a2
Trent Peterson <tpeterson998@west-mec.org>
2019-02-13 13:31:24 -0700

    Final commit
    I fixed the error and commeted my code.

--- sd_mapper.js
+++ sd_mapper.js
@@ -9,11 +9,19 @@
    Date:   2.12.18
 
 */
+// This is where my varaibles are
+// Starts a new date that will keep up eith the current date 
 var thisTime = new Date();
+// This calls the new date
 var timeStr = thisTime.toLocaleString();
+// this grabs the hours and months down from the new date
 var thisHour = thisTime.getHours();
 var thisMonth = thisTime.getMonth();
+// This does some basic math using the hours and months that are grabed earlier 
 var mapNum = (2 * thisMonth + thisHour) % 24;
-var imgStr = `< img src = 'sd_sky${mapNum}.png' / >`;
+// this sets a html tag to be added in when I call tbis varaible
+var imgStr = "<img src = 'sd_sky" + mapNum + ".png' / >";
+// This changes the date in the web site to the new date
 document.getElementById("timeStamp").innerHTML = timeStr;
-document.getElementById("planisphere").insertAdjacentElement("afterbegin", imgStr)
+// This changes the picture in the website depending on the math calculation earlier
+document.getElementById("planisphere").insertAdjacentHTML('afterbegin', imgStr);
